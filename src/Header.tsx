367f80d781ebceae81cd17ae0cfe40c2e2a4632c
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-03-25 16:03:21 +0000

    burger websit set up started and folder structure set up

---
+++ src/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export class Header extends React.PureComponent {
+  render() {
+    return (
+      <div className={'header-container'}>
+        <div className={'logo-container'}>
+          <div>
+            <h2>Build-A-BRGR</h2>
+          </div>
+        </div>
+        <div className={'nb-button-container'}>
+          <div>
+            <button>Home</button>
+          </div>
+          <div>
+            <button>Menu</button>
+          </div>
+          <div>
+            <button>About</button>
+          </div>
+        </div>
+        <div className={'cart-container'}>
+          <div className={'cart-img-container'}></div>
+        </div>
+      </div>
+    );
+  }
+}
