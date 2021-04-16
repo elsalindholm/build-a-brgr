4d22392ea2a7a7025bf1c36b26af11fefedc3e15
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-04-16 11:26:57 +0100

    Assets moved back to root level

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: 'file-loader?name=./Assets/[name].[ext]',
+        use: 'file-loader?name=./assets/[name].[ext]',
       },
       {
         test: /\.svg$/,
